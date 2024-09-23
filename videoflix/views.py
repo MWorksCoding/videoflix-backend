b5702b4f22e91b95f18b404111d3c1438be02c03
MWorksCoding <mail.org@posteo.org>
2024-09-23 21:49:26 +0200

    getting video info local

--- videoflix/views.py
+++ videoflix/views.py
@@ -6,9 +6,13 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
-from rest_framework import status
+from rest_framework import status, viewsets
 from django.core.cache.backends.base import DEFAULT_TIMEOUT
 from django.views.decorators.cache import cache_page
+from django.utils.decorators import method_decorator
+from videoflix.models import Video
+from videoflix.serializers import VideoSerializer
+from django.shortcuts import get_object_or_404
 
 
 # Create your views here.
@@ -17,8 +21,7 @@ CACHE_TTL = getattr(settings, "CACHE_TTL", DEFAULT_TIMEOUT)
 
 class LoginView(ObtainAuthToken):
     def post(self, request, *args, **kwargs):
-        print('self', self)
-        print('request', request)
+
         # serializer takes the incoming data (user / password)
         serializer = self.serializer_class(data=request.data,
                                            context={'request': request})
@@ -47,3 +50,39 @@ class LogoutView(APIView):
         token.delete()
         request.auth.delete()
         return Response({"message": "Successfully logged out."}, status=status.HTTP_200_OK)
+    
+    
+class VideoViewSet(viewsets.ModelViewSet):
+    queryset = Video.objects.all().order_by("created_at")
+    serializer_class = VideoSerializer
+    permission_classes = [IsAuthenticated]
+
+
+class VideoView(APIView):
+    permission_classes = [IsAuthenticated]
+    """
+    VideoView class for handling video-related requests.
+
+    This view supports GET and POST methods for retrieving and creating videos respectively.
+    The view requires the user to be authenticated.
+
+    Methods:
+    - get: Retrieves videos based on visibility (public or private).
+    - post: Creates a new video entry for the authenticated user.
+    """
+
+    # @method_decorator(cache_page(CACHE_TTL))
+class VideoView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, pk=None, format=None):
+        try:
+            if pk:
+                video = get_object_or_404(Video, pk=pk)
+                serializer = VideoSerializer(video, context={'request': request})
+            else:
+                videos = Video.objects.all()
+                serializer = VideoSerializer(videos, many=True, context={'request': request})
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        except Exception as e:
+            return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
--- videoflix_backend/urls.py
+++ videoflix_backend/urls.py
@@ -1,4 +1,4 @@
-"""
+'''
 URL configuration for videoflix_backend project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
@@ -13,10 +13,10 @@ Class-based views
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+'''
 from django.contrib import admin
 from django.urls import include, path
-from videoflix.views import LoginView, LogoutView
+from videoflix.views import LoginView, LogoutView, VideoView
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -25,6 +25,7 @@ urlpatterns = [
     path('login/', LoginView.as_view()),
     path('logout/', LogoutView.as_view()),
     path('api/accounts/', include('authemail.urls')),
-    path("__debug__/", include("debug_toolbar.urls")),
-    path("django-rq/", include("django_rq.urls")),
+    path('__debug__/', include('debug_toolbar.urls')),
+    path('django-rq/', include('django_rq.urls')),
+    path('videos/', VideoView.as_view()),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
